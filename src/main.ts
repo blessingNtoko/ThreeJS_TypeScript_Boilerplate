f1ac1eabac2a8f7a791d9f9849f71c6aebae2505
Blessing <bntoko@gmail.com>
2024-04-20 07:51:37 +0200

    added camera animation

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,46 @@
 import './style.css';
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import Stats from 'three/addons/libs/stats.module.js';
 import { GUI } from 'dat.gui';
 
 const scene = new THREE.Scene();
+scene.add(new THREE.GridHelper());
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, .1, 1000);
-camera.position.z = 1.5;
+camera.lookAt(0, .5, 0);
+
+const cameraPosition1 = new THREE.Vector3(3, .5, 3);
+const cameraPosition2 = new THREE.Vector3(3, .5, -3);
+const cameraPosition3 = new THREE.Vector3(-3, .5, -3);
+const cameraPosition4 = new THREE.Vector3(-3, .5, 3);
+
+camera.position.set(cameraPosition1.x, cameraPosition1.y, cameraPosition1.z);
+
+function camMove() {
+  // Square
+  if (camera.position.x === cameraPosition2.x && camera.position.z > cameraPosition2.z) {
+    camera.position.z -= 0.01;
+  } else if (camera.position.x === cameraPosition2.x && camera.position.z < cameraPosition2.z) {
+    camera.position.set(cameraPosition2.x, cameraPosition2.y, cameraPosition2.z);
+    camera.position.x -= .01;
+  } else if (camera.position.x > cameraPosition3.x && camera.position.z === cameraPosition3.z) {
+    camera.position.x -= .01;
+  } else if (camera.position.x < cameraPosition3.x && camera.position.z === cameraPosition3.z) {
+    camera.position.set(cameraPosition3.x, cameraPosition3.y, cameraPosition3.z);
+    camera.position.z += .01;
+  } else if (camera.position.x === cameraPosition4.x && camera.position.z < cameraPosition4.z) {
+    camera.position.z += .01;
+  } else if (camera.position.x === cameraPosition4.x && camera.position.z > cameraPosition4.z) {
+    camera.position.set(cameraPosition4.x, cameraPosition4.y, cameraPosition4.z);
+    camera.position.x += .01;
+  } else if (camera.position.x < cameraPosition1.x && camera.position.z === cameraPosition1.z) {
+    camera.position.x += .01;
+  } else {
+    camera.position.set(cameraPosition1.x, cameraPosition1.y, cameraPosition1.z);
+  }
+
+
+}
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -19,32 +52,42 @@ window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-new OrbitControls(camera, renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshNormalMaterial({ wireframe: true });
 
 const cube = new THREE.Mesh(geometry, material);
+cube.position.y = .5;
 scene.add(cube);
 
 const stats = new Stats();
 document.body.appendChild(stats.dom);
 
 const gui = new GUI();
-const cubeFolder = gui.addFolder("Cube");
-cubeFolder.add(cube.rotation, "x", 0, Math.PI * 2);
-cubeFolder.add(cube.rotation, "y", 0, Math.PI * 2);
-cubeFolder.add(cube.rotation, "z", 0, Math.PI * 2);
-cubeFolder.open();
 
 const cameraFolder = gui.addFolder("Camera");
-cameraFolder.add(camera.position, "x", 0, 20);
-cameraFolder.add(camera.position, "y", 0, 20);
-cameraFolder.add(camera.position, "z", 0, 20);
+cameraFolder.add(camera.position, "x", -10, 10);
+cameraFolder.add(camera.position, "y", -10, 10);
+cameraFolder.add(camera.position, "z", -10, 10);
+cameraFolder.add(camera, 'fov', 0, 180, .01).onChange(() => {
+  camera.updateProjectionMatrix();
+});
+cameraFolder.add(camera, 'aspect', 0.00001, 10).onChange(() => {
+  camera.updateProjectionMatrix();
+});
+cameraFolder.add(camera, 'near', 0.01, 10).onChange(() => {
+  camera.updateProjectionMatrix();
+});
+cameraFolder.add(camera, 'far', 0.01, 10).onChange(() => {
+  camera.updateProjectionMatrix();
+});
 cameraFolder.open();
 
 function animate() {
   requestAnimationFrame(animate);
+
+  camera.lookAt(cube.position);
+  camMove();
 
   // cube.rotation.x += .01;
   // cube.rotation.y += .01;
