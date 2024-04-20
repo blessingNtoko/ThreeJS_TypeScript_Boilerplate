a241c21c39d322b9f842036ec06732f3a9762875
Blessing <bntoko@gmail.com>
2024-04-20 10:54:39 +0200

    understainding the renderer

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,23 @@
 import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import Stats from 'three/addons/libs/stats.module.js';
-import { GUI } from 'dat.gui';
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, .1, 1000);
+const camera = new THREE.PerspectiveCamera(75, 200 / 200, .1, 1000);
 camera.position.z = 1.5;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 
-window.addEventListener('resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-});
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+renderer.setSize(200, 200);
+// document.body.appendChild(renderer.domElement);
+
+// window.addEventListener('resize', () => {
+//   camera.aspect = window.innerWidth / window.innerHeight;
+//   camera.updateProjectionMatrix();
+//   renderer.setSize(window.innerWidth, window.innerHeight);
+// });
 
 new OrbitControls(camera, renderer.domElement);
 
@@ -27,31 +27,10 @@ const material = new THREE.MeshNormalMaterial({ wireframe: true });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-const stats = new Stats();
-document.body.appendChild(stats.dom);
-
-const gui = new GUI();
-const cubeFolder = gui.addFolder("Cube");
-cubeFolder.add(cube.rotation, "x", 0, Math.PI * 2);
-cubeFolder.add(cube.rotation, "y", 0, Math.PI * 2);
-cubeFolder.add(cube.rotation, "z", 0, Math.PI * 2);
-cubeFolder.open();
-
-const cameraFolder = gui.addFolder("Camera");
-cameraFolder.add(camera.position, "x", 0, 20);
-cameraFolder.add(camera.position, "y", 0, 20);
-cameraFolder.add(camera.position, "z", 0, 20);
-cameraFolder.open();
-
 function animate() {
   requestAnimationFrame(animate);
 
-  // cube.rotation.x += .01;
-  // cube.rotation.y += .01;
-
   renderer.render(scene, camera);
-
-  stats.update();
 }
 
 animate();
